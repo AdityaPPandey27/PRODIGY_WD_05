14c0926b8330990752b0cbde67cd6dd3766e4c3f
Aditya Pratap Pandey <adi239692@gmail.com>
2026-07-16 18:39:33 +0530

    applying the demo API key

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
 // Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API Key
-const API_KEY = "Your_API_Key_Here"; // Replace with your actual OpenWeatherMap API Key
+const API_KEY = "aeaf6607ae8bc5f281219ba7a36e657c"; // Replace with your actual OpenWeatherMap API Key
